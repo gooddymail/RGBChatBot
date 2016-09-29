446b1aaa4d580622c531187600d50bc4091cee65
Katchapon Poolpipat <Yowa@Katchapons-MacBook-Pro.local>
2016-09-29 14:13:54 +0700

    Implement some init method for Keyword and Answer model

--- Sources/App/Models/Answer.swift
+++ Sources/App/Models/Answer.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import Vapor
+import FluentMySQL
+import HTTP
+import Fluent
+
+final class Answer: Model {
+  var id: Node?
+  var time: Int
+  
+  init(time: Int) {
+    self.time = time
+  }
+  
+  init(node: Node, in context: Context) throws {
+    id = try node.extract("id")
+    time = try node.extract("time")
+  }
+  
+  func makeNode(context: Context) throws -> Node {
+    return try Node(node: [
+        "id": id,
+        "time": time
+      ])
+  }
+  
+  static func prepare(_ database: Database) throws {
+    try database.create("answer") { answer in
+      answer.id()
+      answer.int("time")
+    }
+  }
+  
+  static func revert(_ database: Database) throws {
+    try database.delete("answer")
+  }
+}
--- Sources/App/Models/Keyword.swift
+++ Sources/App/Models/Keyword.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import Vapor
+import FluentMySQL
+import HTTP
+import Fluent
+
+final class Keyword: Model {
+  var id: Node?
+  var word: String
+  var createDate: Int
+  
+  init(word: String, createDate: Int) {
+    self.word = word
+    self.createDate = createDate
+  }
+  
+  convenience init(word: String) {
+    let date = Date()
+    self.init(word: word, createDate: Int(date.timeIntervalSince1970))
+  }
+  
+  init(node: Node, in context: Context) throws {
+    id = try node.extract("id")
+    word = try node.extract("word")
+    createDate = try node.extract("create_date")
+  }
+  
+  func makeNode(context: Context) throws -> Node {
+    return try Node(node: [
+        "id": id,
+        "word": word,
+        "create_date": createDate
+      ])
+  }
+  
+  static func prepare(_ database: Database) throws {
+    try database.create("keywords") { keywords in
+      keywords.id()
+      keywords.string("word")
+      keywords.int("create_date")
+    }
+  }
+  
+  static func revert(_ database: Database) throws {
+    try database.delete("keywords")
+  }
+}
